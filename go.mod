e0016d87596d1f01f006a4fae62aaafaf9195048
hosseinnourian <hosseinnourian@protonmail.com>
2024-09-22 21:11:35 +0330

    finish Encrypt function

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module file-encryption
 
 go 1.23.0
+
+require (
+	github.com/go-kit/kit v0.13.0
+	golang.org/x/term v0.24.0
+)
+
+require (
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	golang.org/x/crypto v0.27.0
+	golang.org/x/sys v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"file-encryption/encryptor"
 	"fmt"
 	"os"
+
+	"golang.org/x/term"
 )
 
 func main() {
@@ -24,15 +28,77 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+// done
 func printHelp() {
-
+	fmt.Println("file encryption")
+	fmt.Println("simple file encryption for daily needs.")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("\tgo run . encrypt /path/to/your/file")
+	fmt.Println("")
+	fmt.Println("Commands:")
+	fmt.Println("")
+	fmt.Println("\t encrypt \tEncrypts a file given a password")
+	fmt.Println("\t decrypt\t Tries to decrypt a file using a password")
+	fmt.Println("\t help\t\tDisplay help text")
+	fmt.Println("")
 }
-func encryptHandle() {
 
+func encryptHandle() {
+	if len(os.Args) < 3 {
+		fmt.Println("missing the path to the file.")
+		os.Exit(0)
+	}
+	file := os.Args[2]
+	if !validateFile(file) {
+		panic("File Not Found")
+	}
+	password := getPassword()
+	fmt.Println("\nEncrypting...")
+
+	// use modules
+	encryptor.Encrypt(file, password)
+	fmt.Println("\nFile sucessfully protected")
 }
 func decryptHandle() {
-
+	if len(os.Args) < 3 {
+		fmt.Println("missing the path to the file.")
+		os.Exit(0)
+	}
+	file := os.Args[2]
+	if !validateFile(file) {
+		panic("File Not Found")
+	}
+	fmt.Println("Enter the password")
+	password, _ := term.ReadPassword(0)
+	fmt.Println("\nDecrypting...")
+	// use modules
+	encryptor.Decrypt(file, password)
+	fmt.Println("\nFile sucessfully decrypted")
 }
-func getPassword()  {
-	
+func getPassword() []byte {
+	fmt.Println("Enter Password...")
+	password, _ := term.ReadPassword(0)
+	fmt.Println("Confirm Password...")
+	password2, _ := term.ReadPassword(0)
+
+	if !validatePassword(password, password2) {
+		fmt.Println("\nPassword Is Not Match.")
+		return getPassword()
+	}
+	return password
+}
+func validatePassword(pass1, pass2 []byte) bool {
+	if !bytes.Equal(pass1, pass2) {
+		return false
+	}
+	return true
+}
+func validateFile(file string) bool {
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
