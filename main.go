7dd357472104ee0b68d18794d6ad8438c24defa7
hosseinnourian <hosseinnourian@protonmail.com>
2024-09-22 18:43:20 +0330

    add basic utils functions

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		printHelp()
+		os.Exit(0)
+	}
+	function := os.Args[1]
+
+	switch function {
+	case "help":
+		printHelp()
+	case "encrypt":
+		encryptHandle()
+	case "decrypt":
+		decryptHandle()
+	default:
+		fmt.Println("please run [*]help, [*]encrypt, [*]decrypt")
+		os.Exit(1)
+	}
+}
+func printHelp() {
+
+}
+func encryptHandle() {
+
+}
+func decryptHandle() {
+
+}
+func getPassword()  {
+	
+}
