8f318eabb22c7768e2635b7e1c535e418ba829c7
hosseinnourian <hosseinnourian@protonmail.com>
2024-09-22 21:51:37 +0330

    fix bug in encryption

--- encryptor/encryptor.go
+++ encryptor/encryptor.go
@@ -1,36 +1,36 @@
 package encryptor
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha1"
+	"encoding/hex"
 	"io"
+	"io/ioutil"
 	"os"
 
 	"golang.org/x/crypto/pbkdf2"
 )
 
+// done
 func Encrypt(source string, password []byte) {
+
 	if _, err := os.Stat(source); os.IsNotExist(err) {
 		panic(err.Error())
 	}
 
-	srcFile, err := os.Open(source)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer srcFile.Close()
-
-	plaintext, err := io.ReadAll(srcFile)
+	plaintext, err := ioutil.ReadFile(source)
 
 	if err != nil {
 		panic(err.Error())
 	}
 
 	key := password
-	nonce := make([]byte, 0)
+	nonce := make([]byte, 12)
 
+	// Randomizing the nonce
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
@@ -41,24 +41,74 @@ func Encrypt(source string, password []byte) {
 	if err != nil {
 		panic(err.Error())
 	}
+
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
+
 	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+
+	// Append the nonce to the end of file
 	ciphertext = append(ciphertext, nonce...)
 
-	dstFile, err := os.Create(source)
+	f, err := os.Create(source)
 	if err != nil {
 		panic(err.Error())
 	}
-	defer dstFile.Close()
-	_, err = dstFile.Write(ciphertext)
+	_, err = io.Copy(f, bytes.NewReader(ciphertext))
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
-func Decrypt(source string, password []byte) {
 
+func Decrypt(source string, password []byte) {
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		panic(err.Error())
+	}
+	srcFile, err := os.Open(source)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer srcFile.Close()
+
+	ciphertext, err := io.ReadAll(srcFile)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	key := password
+	salt := ciphertext[len(ciphertext)-12:]
+	str := hex.EncodeToString(salt)
+	nonce, err := hex.DecodeString(str)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	plainetext, err := aesgcm.Open(nil, nonce, ciphertext[:len(ciphertext)-12], nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	destFile, err := os.Create(source)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer destFile.Close()
+
+	_, err = destFile.Write(plainetext)
+	if err != nil {
+		panic(err.Error())
+	}
 }
